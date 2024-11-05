c6762822397def9d8417f49516181f3a67adc46a
Tuni <cbsenevir@att.net>
2024-11-04 23:41:02 -0500

    Lab 4 Assignment 6

--- labs/src/state/state.py
+++ labs/src/state/state.py
@@ -15,6 +15,7 @@ class state:
         self.sub = rospy.Subscriber('/gazebo/link_states', LinkStates, self.pub)
         self.pose_pub = rospy.Publisher('/jackal/ground_truth/pose', PoseStamped, queue_size=1)
         self.twist_pub = rospy.Publisher('/jackal/ground_truth/twist', TwistStamped, queue_size=1)
+<<<<<<< HEAD
         self.cmd_vel_pub = rospy.Publisher('/jackal_velocity_controller/cmd_vel', Twist, queue_size = 1)
         self.move_bot = Twist()
         self.move_bot.linear.x = 0
@@ -22,6 +23,20 @@ class state:
 
     def pub(self, data):
         now = rospy.get_rostime()
+=======
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
+        self.move_bot = Twist()
+        self.move_bot.linear.x = 1
+        self.move_bot.angular.z = 0.5
+
+
+    def pub(self, data):
+        now = rospy.get_rostime()
+        for i in range(len(data.name)):
+            if data.name[i] == 'jackal::base_link':
+                link = i
+                break
+>>>>>>> Lab 4 Assignment 6
         pose = PoseStamped(
             header = Header(
 	        stamp = now,
@@ -32,6 +47,7 @@ class state:
 
         twist = TwistStamped(
             header = Header(
+<<<<<<< HEAD
 	        stamp = now,
 	        frame_id = 'base_link'
 	    ),
@@ -39,8 +55,16 @@ class state:
         )
 
         #self.cmd_vel_pub.publish(self.move_bot)
+=======
+                stamp = now,
+                frame_id = 'base_link'
+            ),
+            twist = data.twist[link]
+        )
+>>>>>>> Lab 4 Assignment 6
         self.pose_pub.publish(pose)
         self.twist_pub.publish(twist)
+        self.cmd_vel_pub.publish(self.move_bot)
 
 def main():
     rospy.init_node('state')
