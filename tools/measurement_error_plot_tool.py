f696a136430a5dcba89a1d0085e5ecc6331e36ca
eins <gz2355@columbia.edu>
2024-12-02 21:52:44 -0500

    Plotting tool for Lab6 A1 under folder tools

---
+++ tools/measurement_error_plot_tool.py
@@ -0,0 +1,39 @@
+import numpy as np
+import plotly.graph_objects as go
+import pandas as pd
+import os
+from pathlib import Path
+
+current_dir = Path(__file__).resolve()
+data_path = current_dir.parent.parent
+
+# Read the data from the text file
+file_name = str(data_path)+"/labs/datalog/cyan.txt"
+data = np.loadtxt(file_name, delimiter=",")
+
+# Create a plotly figure
+fig = go.Figure()
+
+# Add traces for each column
+for i in range(data.shape[1]):
+    if i % 2 == 0:
+        axis = 'x'
+    elif i % 2 == 1:
+        axis = 'y'
+    fig.add_trace(go.Scatter(
+        y=data[:, i],
+        mode='lines',
+        name=f'{axis}_{i//2+1}'
+    ))
+
+# Update layout for better visualization
+fig.update_layout(
+    title="Cyan",
+    xaxis_title="Index",
+    yaxis_title="Value",
+    legend_title="Columns",
+    template="plotly"
+)
+
+# Show the plot
+fig.show()
